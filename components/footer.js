227353ad611de9b13b92c86e356905e79252cffa
Justin <justinafsari@justinafsari.lan>
2022-05-14 17:03:56 -0400

    third-commit

--- components/footer.js
+++ components/footer.js
@@ -2,7 +2,6 @@ import Container from './container'
 import { EXAMPLE_PATH } from '../lib/constants'
 
 export default function Footer() {
-  {console.log('hi')}
   return (
     <footer className="bg-accent-1 border-t border-accent-2">
       <Container>
